23564b2136cf0212c6000560e2558c0668989040
Devansh-Patel-2929 <dp4555@srmist.edu.in>
2025-10-05 00:15:35 +0530

    Added get_average_rating Function

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 from azure.cosmos import CosmosClient
 from typing import Dict, Any
 
-import os
 from dotenv import load_dotenv
+import streamlit as st
+import os
+
 load_dotenv()
 
 subscription_key = os.getenv("subscription_key")
@@ -55,7 +57,26 @@ def vector_search(query_text,top_k=5):
         print("Error in vector search: {e} could not find vector")
         return []
     
+
+def get_average_rating():
+    db_query = """
+        SELECT VALUE AVG(StringToNumber(c.rating))
+        FROM c
+        WHERE IS_DEFINED(c.rating) AND IS_STRING(c.rating) AND NOT IS_NULL(c.rating)
+    """
+    results = container.query_items(
+        query=db_query,
+        enable_cross_partition_query=True
+    )
+    try:
+        average_rating = next(results) 
+        return average_rating if average_rating is not None else 0.0
+    except StopIteration:
+        return 0.0
+    
 query = "Find action movies with sad ending"
 results = vector_search(query,top_k=3)
 for idx, item in enumerate(results):
     print(item)
+
+print(f"Average rating of all movies: {get_average_rating()}")
