3da3a80ae30198757c85e2b4e4754f5b5b55e9f3
Devansh-Patel-2929 <dp4555@srmist.edu.in>
2025-10-05 11:23:04 +0530

    added get similar movies function

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import os
 
 load_dotenv()
 
-subscription_key = os.getenv("subscription_key")
 EMBEDDING_MODEL_ENDPOINT = os.getenv("EMBEDDING_MODEL_ENDPOINT")
 COSMOS_CONNECTION_STRING = os.getenv("COSMOS_CONNECTION_STRING")
 CONTAINER_NAME = os.getenv("CONTAINER_NAME")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
+subscription_key = os.getenv("subscription_key")
 
 cosmos_client = CosmosClient.from_connection_string(COSMOS_CONNECTION_STRING)
 database = cosmos_client.get_database_client(DATABASE_NAME)
@@ -34,8 +34,11 @@ def vector_search(query_text,top_k=5):
     db_query = """
         SELECT TOP @top_k
             c.title,
-            c.genres,
+            c.genre,
             c.rating,
+            c.release_year,
+            c.plot_summary,
+            c.plot_synopsis,
             VectorDistance(c.embedding, @query_embedding) AS similarity_score
         FROM c
         ORDER BY VectorDistance(c.embedding, @query_embedding)
@@ -58,25 +61,97 @@ def vector_search(query_text,top_k=5):
         return []
     
 
-def get_average_rating():
-    db_query = """
-        SELECT VALUE AVG(StringToNumber(c.rating))
-        FROM c
-        WHERE IS_DEFINED(c.rating) AND IS_STRING(c.rating) AND NOT IS_NULL(c.rating)
-    """
-    results = container.query_items(
-        query=db_query,
-        enable_cross_partition_query=True
-    )
-    try:
-        average_rating = next(results) 
-        return average_rating if average_rating is not None else 0.0
-    except StopIteration:
-        return 0.0
+# def get_average_rating():
+#     db_query = """
+#         SELECT VALUE AVG(StringToNumber(c.rating))
+#         FROM c
+#         WHERE IS_DEFINED(c.rating) AND IS_STRING(c.rating) AND NOT IS_NULL(c.rating)
+#     """
+#     results = container.query_items(
+#         query=db_query,
+#         enable_cross_partition_query=True
+#     )
+#     try:
+#         average_rating = next(results) 
+#         return average_rating if average_rating is not None else 0.0
+#     except StopIteration:
+#         return 0.0
+
     
+def get_embedding(container, movie_name: str):
+    find_movie_query = """
+        SELECT VALUE c.embedding 
+        FROM c
+        WHERE c.title = @movie_name
+    """
+    parameters = [ 
+        {"name": "@movie_name", "value": movie_name}
+    ]
+
+    try:
+        result_items = list(container.query_items(
+            query=find_movie_query,
+            parameters=parameters,
+            enable_cross_partition_query=True
+        ))
+        if result_items:
+            return result_items[0]
+        else:
+            return None
+            
+    except Exception as e:
+        print(f"Error fetching embedding for movie '{movie_name}': {e}")
+        return None
+
+
+def find_similar(movie_name: str , top_k=5):
+    query_embedding = get_embedding(container,movie_name)
+    if query_embedding is None:
+        print(f"Error: Could not find embedding for source movie '{movie_name}'.")
+        return []
+    
+    db_query = """
+        SELECT TOP @num_results
+            c.title,
+            c.genre,
+            c.rating,
+            c.release_year,
+            c.plot_summary,
+            c.plot_synopsis,
+            VectorDistance(c.embedding, @embedding) AS similarity_score
+        FROM c
+        ORDER BY VectorDistance(c.embedding, @embedding)
+    """
+    parameters =[
+        {"name": "@embedding", "value": query_embedding},
+        {"name": "@num_results", "value": top_k + 1}
+    ]
+
+    try:
+        results = list(container.query_items(
+            query=db_query,
+            parameters=parameters,
+            enable_cross_partition_query=True
+        ))
+
+        filtered_results = [
+            result for result in results 
+            if result['title'] != movie_name
+        ]
+
+        return filtered_results[:top_k]
+        
+    except Exception as e:
+        print(f"Error in finding similar movies: {e}")
+        return []
+
+
 query = "Find action movies with sad ending"
 results = vector_search(query,top_k=3)
 for idx, item in enumerate(results):
     print(item)
 
-print(f"Average rating of all movies: {get_average_rating()}")
+movie = "Wet Hot American Summer"
+
+results1 = find_similar(movie,5)
+print (results1)
